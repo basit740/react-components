24b718126ede810b710ee54cc95050f6b199854e
Basit Ali <alib4111@gmail.com>
2022-08-30 09:00:28 +0500

    removed netlify warning

--- src/data/slides.js
+++ src/data/slides.js
@@ -12,7 +12,7 @@ const slides = [
 		testimonialBody:
 			'Classes with Basit have been fun. His way of teaching is patient and listen to all questions I have and making difficult things easier so that one can understand and remember everything. I recommend him to everyone.',
 		testimonialAuthor: 'Wosen',
-		testimonialDate: 'August 20, 2022',
+		testimonialDate: 'August 21, 2022',
 		img: '../../imgs/second.jpg',
 	},
 	{
@@ -20,7 +20,7 @@ const slides = [
 		testimonialBody:
 			'Basit was very flexible on the time of meeting given we are on opposite sides of the globe. He took the time to really understand my application and what I was trying to achieve, and provided much needed feedback concerning other parts of the application. Will definitely request his service again if I am stuck',
 		testimonialAuthor: 'Graig',
-		testimonialDate: 'August 20, 2022',
+		testimonialDate: 'August 22, 2022',
 		img: '../../imgs/third.jpg',
 	},
 	{
@@ -28,7 +28,7 @@ const slides = [
 		testimonialBody:
 			'Basit was great! He was very communicative and flexible with timing. He is a talented developer and took the time to answer my questions! He did great work and I look forward to working with him again!',
 		testimonialAuthor: 'Denny',
-		testimonialDate: 'August 20, 2022',
+		testimonialDate: 'August 23, 2022',
 		img: '../../imgs/fourth.jpg',
 	},
 ];
